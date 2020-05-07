53816dd42bcdb948ca205ab00463b4761e9ff4fd
guoywcode <943364123@qq.com>
2020-05-07 18:34:26 +0800

    "商品分类"

--- src/api/productCategory.js
+++ src/api/productCategory.js
@@ -2,6 +2,23 @@ import request from '@/utils/request'
 
 export function getList() {
   return request({
-    url: 'api/productCategory/list/withChildren'
+    url: 'api/productCategory/list/withChildren',
+    method: 'get'
+  })
+}
+
+export function updateNavStatus(data) {
+  return request({
+    url: 'api/productCategory/updateNavStatus',
+    method: 'post',
+    data: data
+  })
+}
+
+export function updateShowStatus(data) {
+  return request({
+    url: 'api/productCategory/updateShowStatus',
+    method: 'post',
+    data: data
   })
 }
--- src/views/pms/productCategory/list.vue
+++ src/views/pms/productCategory/list.vue
@@ -1,44 +1,52 @@
 <template>
   <div class="app-container">
-    <div >
-    <el-table
-      :data="list"
-      style="width:100%;margin-bottom:20px"
-      row-key="id"
-      border
-      default-expand-all
-      :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-      <el-table-column prop="id" label="编号" width="100"></el-table-column>
-      <el-table-column prop="name" label="分类名称" width="180"></el-table-column>
-      <el-table-column prop="level" label="级别"></el-table-column>
-      <el-table-column prop="productCount" label="商品数量"></el-table-column>
-      <el-table-column prop="productUnit" label="数量单位"></el-table-column>
-      <el-table-column prop="navStatus" label="导航栏"></el-table-column>
-      <el-table-column prop="showStatus" label="是否显示"></el-table-column>
-      <el-table-column prop="sort" label="排序"></el-table-column>
-      <el-table-column label="设置"></el-table-column>
-      <el-table-column label="操作"></el-table-column>
-    </el-table>
+    <div>
+      <el-table :data="list" style="width:100%;margin-bottom:20px" row-key="id" :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+        <el-table-column prop="id" label="编号" width="80"></el-table-column>
+        <el-table-column prop="name" label="分类名称" width="180"></el-table-column>
+        <el-table-column prop="level" label="级别"></el-table-column>
+        <el-table-column prop="productCount" label="商品数量"></el-table-column>
+        <el-table-column prop="productUnit" label="数量单位"></el-table-column>
+        <el-table-column label="导航栏">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.navStatus"
+              :active-value="1"
+              :inactive-value="0"
+              @change="handleNavStatusChange(scope.$index, scope.row)"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否显示">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.showStatus"
+              :active-value="1"
+              :inactive-value="0"
+              @change="handleShowStatusChange(scope.$index, scope.row)"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column prop="sort" label="排序"></el-table-column>
+        <el-table-column label="设置"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
-  import { getList} from '@/api/productCategory'
+  import {getList, updateNavStatus,updateShowStatus} from '@/api/productCategory'
   
   export default {
     name:"",
     data(){
       return {
         list:[],
-        query:{
-        
-        },
+        query:{},
         page:{
-          pageNum: 1,
-          pageSize: 5
+          pageNum:1,
+          pageSize:5
         },
-        total: null,
+        total:null,
         listLoading:false
       }
     },
@@ -48,11 +56,39 @@
     methods:{
       getList(){
         this.listLoading = true;
-        getList().then(response =>{
+        getList().then(response => {
           console.log(response)
           this.listLoading = false;
           this.list = response.data;
           this.total = response.data.total;
+        })
+      },
+      handleNavStatusChange(index, row){
+        let data = new URLSearchParams();
+        let ids = [];
+        ids.push(row.id);
+        data.append("ids", ids);
+        data.append("navStatus", row.navStatus)
+        updateNavStatus(data).then(response => {
+          this.$message({
+            message:'修改成功',
+            type:'success',
+            duration:1000
+          });
+        })
+      },
+      handleShowStatusChange(index, row){
+        let data = new URLSearchParams();
+        let ids = [];
+        ids.push(row.id);
+        data.append("ids", ids);
+        data.append("showStatus", row.showStatus)
+        updateShowStatus(data).then(response => {
+          this.$message({
+            message:'修改成功',
+            type:'success',
+            duration:1000
+          });
         })
       }
     }
@@ -60,5 +96,11 @@
 </script>
 
 <style scoped>
-
+  /deep/ .el-table th > .cell{
+    text-align:center;
+  }
+  
+  /deep/ .el-table .cell{
+    text-align:center;
+  }
 </style>
